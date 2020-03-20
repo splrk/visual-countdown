e4e5bc4a0f72fd88a829b6e033e2b2886b5008b1
Ryan Seal <ryan@code.splrk.net>
2020-03-20 06:19:49 +0200

    Move command line parsing to to Struct

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,44 @@ fn main() {
     let mut args: Args = env::args();
     args.next();
 
-    let seconds = parse_args(args);
-    
-    println!("{:?}", seconds);
+    let config = Config::new(args);
+
+    if config.filename.is_some() {
+        println!("{}", config.filename.as_ref().unwrap());
+    }
+    println!("{:?}", config.seconds);
 }
 
-fn parse_args(mut args: Args) -> u32 {
-    let mut time_str = String::new();
-    loop {
-        let arg = args.next();
-        if arg.is_none() {
-            break;
+struct Config {
+    seconds: u32,
+    filename: Option<String>,
+}
+
+impl Config {
+    fn new(mut args: Args) -> Config {
+        let mut time_str = String::new();
+        let mut filename: Option<String> = None;
+
+        loop {
+            let arg = args.next();
+            if arg.is_none() {
+                break;
+            }
+
+            let arg = arg.unwrap();
+            if arg == "-f" {
+                let path = args.next().expect("Must provide a filename").clone();
+                filename = Some(path.clone());
+                
+                time_str = fs::read_to_string(path).expect("Unable to read file");
+                time_str = String::from(time_str.trim());
+            } else {
+                time_str = arg;
+            }
         }
 
-        let arg = arg.unwrap();
-        if arg == "-f" {
-            let filename = args.next().expect("Must provide a filename");
-            time_str = fs::read_to_string(filename).expect("Unable to read file");
-            time_str = String::from(time_str.trim());
-        } else {
-            time_str = arg;
-        }
+        let seconds: u32 = time_str.parse().expect("Can only take an integer");
+
+        Config { filename, seconds }
     }
-
-    time_str.parse().expect("Can only take an integer")
 }
